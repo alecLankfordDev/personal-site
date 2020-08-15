92a2dccc9cfef9d1cfbec029740e8d42b71bb96e
Alec Lankford <me@aleclankford.dev>
2020-08-15 18:22:04 -0500

    action bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActionBarComponent } from './action-bar/action-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ActionBarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
