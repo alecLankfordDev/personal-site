6601b89b6974d2ba010cfb97c98269c23255a91b
Alec Lankford <me@aleclankford.dev>
2020-08-15 18:40:17 -0500

    Navbar, title
    Added boilerplate navbar (to be changed) and a sliding title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActionBarComponent } from './action-bar/action-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      ActionBarComponent
+      ActionBarComponent,
+      HomeComponent
    ],
   imports: [
     BrowserModule,
