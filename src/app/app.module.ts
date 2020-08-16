de6ef02db7307ff67938cba76346afb70584d36a
Alec Lankford <me@aleclankford.dev>
2020-08-15 20:31:38 -0500

    font, home-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActionBarComponent } from './action-bar/action-bar.component';
 import { HomeComponent } from './home/home.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [			
     AppComponent,
       ActionBarComponent,
-      HomeComponent
+      HomeComponent,
+      HomeContentComponent
    ],
   imports: [
     BrowserModule,
